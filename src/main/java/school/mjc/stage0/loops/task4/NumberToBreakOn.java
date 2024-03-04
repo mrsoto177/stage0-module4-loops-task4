e1e91882d98f7a59418020750dcf103df4365551
Marcos <msoto@hopewellsystem.com>
2024-03-03 23:24:39 -0600

    M4 Loops t4 2

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -3,39 +3,31 @@ package school.mjc.stage0.loops.task4;
 public class NumberToBreakOn {
 	public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
 		boolean mensajeImpreso = false;
-		
-		
-		if(toBreakWith < numberToGoUntil) {
+
+		if (toBreakWith < numberToGoUntil) {
 			for (int i = 1; i < numberToGoUntil; i++) {
 				System.out.println(i);
 				if (i == toBreakWith) {
-					
+
 					break;
 				}
 			}
 		}
-		
-		
+
 		else if (numberToGoUntil < toBreakWith) {
-				{
-					System.out.println("iterating till the end");
-					
-					
-					for (int i = 1; i <= toBreakWith; i++) {
-						System.out.println(i);
-						if (i == numberToGoUntil) {
-							
-							break;
-						}
+			{
+				System.out.println("iterating till the end");
+
+				for (int i = 1; i <= toBreakWith; i++) {
+					System.out.println(i);
+					if (i == numberToGoUntil) {
+
+						break;
 					}
 				}
 			}
-		
-		 
+		}
 
-		
-		
-		
 	}
 
 	public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -4,23 +4,26 @@ public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
     	int sum = numberToSkip + lastInRow;
     	int counted = 0;
-    	for(int i=0; i<=lastInRow;i++) {
-    		counted  +=i;
-    		if(numberToSkip > lastInRow ) {
-    			System.out.println("number to skip is bugger than the last");
-    			break;
-    		} else if(lastInRow < 0){
-    			System.out.println("last number in row is negative");
-    			break;
-    		} else {
-    			System.out.println("skipped sum is " + sum + " counted sum is " + counted);
-    			break;
-    		}
-    	}
+    	
+    	if (lastInRow < 0 || numberToSkip < 0) {
+    		System.out.println("last number in row is negative");
+    		
+    	} else if(numberToSkip > lastInRow ) {
+			System.out.println("number to skip is bigger then the last");
+			
+		} else if (lastInRow > numberToSkip) {
+			for(int i=numberToSkip; i<=lastInRow;i++) {
+	    		counted  +=i;
+		
+	    	}
+			System.out.println("skipped sum is " + sum + " counted sum is " + counted);
+		}
+    	
+    	
     }
     public static void main(String[] args) {
     	TwoRangesSum sum = new TwoRangesSum();
-    	sum.printSumOfTwoRanges(90, 90);
+    	sum.printSumOfTwoRanges(-50, -10);
     }
     
 }
